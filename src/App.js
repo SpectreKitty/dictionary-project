d13a4176ecdef801c25472ae6a40dd372cbf821d
SpectreKitty <astry.brana@gmail.com>
2024-01-29 14:52:00 -0500

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import logo from "./shecodeslogo.png";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Astry Brana, open-sourced on{" "}
+          <a href="https://github.com/SpectreKitty/dictionary-project">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dictionary-project-by-astry.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
