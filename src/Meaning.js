6187cde56e0781a0bf857017acf06893ee6e29db
SpectreKitty <astry.brana@gmail.com>
2024-01-30 12:21:53 -0500

    pulled synonyms from api into separate component also made example its own component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
+import Examples from "./Examples.js";
 
 export default function Meaning(props) {
   console.log(props.meanings);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
       <p>
-        <em>{props.meaning.example}</em>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
       </p>
+      <Examples example={props.meaning.example} />
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
